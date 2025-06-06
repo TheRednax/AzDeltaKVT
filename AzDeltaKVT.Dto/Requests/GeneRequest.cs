b87a4f42cf1e5e310f4915a02d142e7ca61cfbc8
turbello <jonathan.bollaert@student.vives.be>
2025-06-06 12:46:28 +0200

    added nullables in dto

--- AzDeltaKVT.Dto/Requests/GeneRequest.cs
+++ AzDeltaKVT.Dto/Requests/GeneRequest.cs
@@ -10,12 +10,12 @@ namespace AzDeltaKVT.Dto.Requests
 	public class GeneRequest
     {
         public string Name { get; set; }
-        public string Chromosome { get; set; }
-        public int Start { get; set; }
-        public int Stop { get; set; }
-        public string UserInfo { get; set; }
+        public string? Chromosome { get; set; }
+        public int? Start { get; set; }
+        public int? Stop { get; set; }
+        public string? UserInfo { get; set; }
 
-        public string Nm_Number { get; set; }
+        public string? Nm_Number { get; set; }
         public int? Position { get; set; }
     }
 }
--- AzDeltaKVT.Dto/Requests/GeneVariantRequest.cs
+++ AzDeltaKVT.Dto/Requests/GeneVariantRequest.cs
@@ -12,11 +12,11 @@ namespace AzDeltaKVT.Dto.Requests
         public string NmId { get; set; }
         public NmTranscript NmTranscript { get; set; }
 
-        public int VariantId { get; set; }
-        public Variant Variant { get; set; }
+        public int? VariantId { get; set; }
+        public Variant? Variant { get; set; }
 
-        public string BiologicalEffect { get; set; }
-        public string Classification { get; set; }
-        public string UserInfo { get; set; }
+        public string? BiologicalEffect { get; set; }
+        public string? Classification { get; set; }
+        public string? UserInfo { get; set; }
     }
 }
