a32d92deaaa80410df58896628fe0bf3bac19b2c
turbello <jonathan.bollaert@student.vives.be>
2025-06-06 14:41:02 +0200

    Changed DTO, removing heritance

--- AzDeltaKVT.Dto/Results/VariantResult.cs
+++ AzDeltaKVT.Dto/Results/VariantResult.cs
@@ -9,6 +9,13 @@ using AzDeltaKVT.Dto.Results;
 namespace AzDeltaKVT.Dto.Results
 {
 	public class VariantResult : Variant
-	{
-	}
+    {
+        public int VariantId { get; set; }
+        public string Chromosome { get; set; }
+        public int? Position { get; set; }
+        public string? Alternative { get; set; }
+
+        public string? Reference { get; set; }
+        public string? UserInfo { get; set; }
+    }
 }
