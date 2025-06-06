4ec315564ddb86a03cb36e15c5342d9f7c72292a
Nicolas-Goemanne <nicolas.goemanne@live.nl>
2025-06-06 10:32:26 +0200

    Adjusted Program.cs so it reached the api in the docker

--- AzDeltaKVT.UI/Program.cs
+++ AzDeltaKVT.UI/Program.cs
@@ -1,4 +1,5 @@
 using AzDeltaKVT.UI;
+using AzDeltaKVT.UI.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,20 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Configure HttpClient voor Docker backend
+builder.Services.AddScoped(sp =>
+{
+    var handler = new HttpClientHandler()
+    {
+        ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+    };
+    return new HttpClient(handler)
+    {
+        BaseAddress = new Uri("https://localhost:32772/")
+    };
+});
+
+// Register ApiService
+builder.Services.AddScoped<ApiService>();
 
 await builder.Build().RunAsync();
