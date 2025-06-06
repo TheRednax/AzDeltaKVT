a1c266fff22147b3ce14deb238fb6681817c667d
TheRednax <milanderuytter@gmail.com>
2025-06-06 09:03:46 +0200

    Basic Dtos

---
+++ AzDeltaKVT.Dto/Requests/GeneRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AzDektaKVT.Model;
+
+namespace AzDeltaKVT.Dto.Requests
+{
+	public class GeneRequest : Gene
+	{
+		public string Nm_Number { get; set; }
+	}
+}
---
+++ AzDeltaKVT.Dto/Results/VariantResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AzDektaKVT.Model;
+using AzDeltaKVT.Dto.Results;
+
+namespace AzDeltaKVT.Dto.Results
+{
+	public class VariantResult : Variant
+	{
+	}
+}
