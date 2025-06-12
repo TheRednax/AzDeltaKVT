e61114bcfd1a7a576985ab141f204b8a87a3011c
turbello <jonathan.bollaert@student.vives.be>
2025-06-12 13:18:17 +0200

    Docker setup and yml sleep timer

--- AzDeltaKVT.API/Program.cs
+++ AzDeltaKVT.API/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddDbContext<AzDeltaKVTDbContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -34,49 +34,16 @@ builder.Services.AddScoped<UploadService>();
 
 var app = builder.Build();
 
-
-//Set seed to true to seed the database with initial data
-//Don't forget to set it to false after the first run to duplicates
-
 using var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<AzDeltaKVTDbContext>();
 
-var maxRetries = 10;
-var delay = TimeSpan.FromSeconds(5);
+context.Database.EnsureCreated();
 
-for (int i = 0; i < maxRetries; i++)
+if (app.Environment.IsDevelopment())
 {
-    try
-    {
-        context.Database.CanConnect();
-        break;
-    }
-    catch (Exception ex) when (i < maxRetries - 1)
-    {
-        //logger.LogWarning($"Database not ready, attempt {i + 1}/{maxRetries}. Waiting {delay.TotalSeconds}s...");
-        await Task.Delay(delay);
-    }
-}
-
-try
-{
-    context.Database.EnsureCreated();
-
-    if (!context.Genes.Any())
-    {
-        context.Seed();
-    }
-}
-catch (Exception ex)
-{
-    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "Error during migration or seeding");
-    throw;  // optionally rethrow to crash app so you see error in logs
-}
-
-app.UseSwagger();
+    app.UseSwagger();
     app.UseSwaggerUI();
-
+}
 
 app.UseCors("AllowAll");
 
